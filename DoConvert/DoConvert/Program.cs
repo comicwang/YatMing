f3c818b2a1e33b952627d33bb2162edc9fcb978c
comicwang <672802899@qq.com>
2016-06-16 17:36:49 +0800

    office文件转换后台

---
+++ DoConvert/DoConvert/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace DoConvert
+{
+    class Program
+    {
+        private static string _pdfFolder = ConfigurationManager.AppSettings["pdfFolder"];
+        private static string _swfFolder = ConfigurationManager.AppSettings["swfFolder"];
+
+        static void Main(string[] args)
+        {
+            if (args == null || args.Length == 0)
+                return;
+            string filePath = args[0];
+           // string filePath = @"C:\Users\Administrator\Desktop\新建文件夹 (2)\微分销.ppt";
+            string pdfPath = Path.Combine(_pdfFolder, Path.GetFileNameWithoutExtension(filePath) + ".pdf");
+            string swfPath = Path.Combine(_swfFolder, Path.GetFileNameWithoutExtension(filePath) + ".swf");
+            if (filePath.ToLower().EndsWith(".doc") || filePath.ToLower().EndsWith(".docx"))
+            {
+                Console.WriteLine("开始转换DOC...");
+                bool pdfSuccess = OfficeParser.ConvertDocToPdF(filePath, pdfPath);
+                if (pdfSuccess)
+                {
+                    Console.WriteLine("开始转换SWF...");
+                    bool swfSuccess = OfficeParser.PDFConvertToSwf(pdfPath, swfPath);
+                    if (swfSuccess)
+                    {
+                        Console.WriteLine("转换完成！");
+                    }
+                    else
+                    {
+                        Console.WriteLine(string.Format("转换{0}为SWF失败", Path.GetFileName(filePath)));
+                    }
+                }
+                else
+                {
+                    Console.WriteLine(string.Format("转换{0}为PDF失败", Path.GetFileName(filePath)));
+                }
+            }
+            else if (filePath.ToLower().EndsWith(".ppt") || filePath.ToLower().EndsWith(".pptx"))
+            {
+                Console.WriteLine("开始转换PPT...");
+                bool pdfSuccess = OfficeParser.ConvertPowerPointToPdf(filePath, pdfPath);
+                if (pdfSuccess)
+                {
+                    Console.WriteLine("开始转换SWF...");
+                    bool swfSuccess = OfficeParser.PDFConvertToSwf(pdfPath, swfPath);
+                    if (swfSuccess)
+                    {
+                        Console.WriteLine("转换完成！");
+                    }
+                    else
+                    {
+                        Console.WriteLine(string.Format("转换{0}为SWF失败", Path.GetFileName(filePath)));
+                    }
+                }
+                else
+                {
+                    Console.WriteLine(string.Format("转换{0}为PDF失败", Path.GetFileName(filePath)));
+                }
+            }
+            else if (filePath.ToLower().EndsWith(".xls"))
+            {
+                Console.WriteLine("开始转换XLS...");
+                bool pdfSuccess = OfficeParser.ConvertExcelToPdf(filePath, pdfPath);
+                if (pdfSuccess)
+                {
+                    Console.WriteLine("开始转换SWF...");
+                    bool swfSuccess = OfficeParser.PDFConvertToSwf(pdfPath, swfPath);
+                    if (swfSuccess)
+                    {
+                        Console.WriteLine("转换完成！");
+                    }
+                    else
+                    {
+                        Console.WriteLine(string.Format("转换{0}为SWF失败", Path.GetFileName(filePath)));
+                    }
+                }
+                else
+                {
+                    Console.WriteLine(string.Format("转换{0}为PDF失败", Path.GetFileName(filePath)));
+                }
+            }
+            else
+            {
+                Console.WriteLine("文件格式不支持！");
+            }
+           // Console.ReadKey();
+        }
+    }
+}
---
+++ DoConvert/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "DOC文件|*.doc|PPT文件|*.ppt";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                System.Diagnostics.Process.Start(@"C:\Users\Administrator\Desktop\DoConvert\DoConvert\bin\Debug\DoConvert.exe", dialog.FileName);
+            }
+        }
+    }
+}
