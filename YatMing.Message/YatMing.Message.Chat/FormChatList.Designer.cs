a306398f7fd1d4b9f594992fd213304cc8fe3e46
comicwang <672802899@qq.com>
2016-07-06 17:52:01 +0800

    Add

---
+++ YatMing.Message/YatMing.Message.Chat/FormChatList.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace YatMing.Message.Chat
+{
+    partial class FormChatList
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstFriend = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // lstFriend
+            // 
+            this.lstFriend.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lstFriend.FormattingEnabled = true;
+            this.lstFriend.ItemHeight = 12;
+            this.lstFriend.Location = new System.Drawing.Point(0, 0);
+            this.lstFriend.Name = "lstFriend";
+            this.lstFriend.Size = new System.Drawing.Size(241, 469);
+            this.lstFriend.TabIndex = 0;
+            this.lstFriend.DoubleClick += new System.EventHandler(this.lstFriend_DoubleClick);
+            // 
+            // FormChatList
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(241, 469);
+            this.Controls.Add(this.lstFriend);
+            this.Name = "FormChatList";
+            this.Text = "FormChatList";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormChatList_FormClosing);
+            this.Load += new System.EventHandler(this.FormChatList_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstFriend;
+    }
+}
